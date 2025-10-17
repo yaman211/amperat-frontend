625ac9b4153bf5e0f5ad47ade44e51fa1788924c
yaman211 <yamanwaza@gmail.com>
2025-10-17 17:21:40 +0300

    recaptcha

--- quasar.config.ts
+++ quasar.config.ts
@@ -75,8 +75,9 @@ export default defineConfig((ctx) => {
       // "chain" is a webpack-chain object https://github.com/sorrycc/webpack-chain
       // chainWebpack (/* chain, { isClient, isServer } */) {}
       env: {
-        BASE_URL: 'http://192.168.1.6:3000',
+        BASE_URL: 'http://192.168.1.4:3000',
         VERSION: '1.0.0',
+        RECAPTCHA_SITE_KEY: '6Lc4ke0rAAAAAAMACtD1xV1EWuQdnYFV02Cr-VIK',
         // BASE_URL: 'http://127.0.0.1:3000',
         // BASE_URL: 'http://amperat-dev-4.eu-4.evennode.com',
       },
