8b09396e86377cf7a92458df4a821bba2d8eb617
yaman211 <yamanwaza@gmail.com>
2025-10-21 20:46:05 +0300

    barcode box and sector

--- src/modules/management/endpoints.ts
+++ src/modules/management/endpoints.ts
@@ -7,8 +7,7 @@ export const CLOCK_INVOICE_BY_ID = (clockId: number) => `/invoices/${clockId}`;
 export const INVOICE_BY_ID = (id: number) => `/invoices/details/${id}`;
 export const REVERT_LAST_INVOICE = (clockId: number) => `/invoices/${clockId}/revert-last-invoice`;
 export const CLOCKS_BY_BOX = (boxId: number) => `/boxes/${boxId}/clocks`;
-export const CLOCKS_BY_BOX_BY_NUMBER = (boxNumber: number) =>
-  `/boxes/${boxNumber}/clocks-by-box-number`;
+export const CLOCKS_BY_BOX_BY_ID = (id: number) => `/boxes/${id}/clocks-by-box-id`;
 
 export const SECTOR_BY_ID = (id: number) => `/sectors/${id}`;
 export const SECTORS = '/sectors';
